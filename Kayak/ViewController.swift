2f8c7f7628d6945a64191b58a333ac730b29822a
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2019-03-23 23:19:05 -0700

    Dolphin sounds in

--- Kayak/ViewController.swift
+++ Kayak/ViewController.swift
@@ -13,13 +13,20 @@ import ARKit
 public class ViewController: UIViewController, ARSCNViewDelegate {
     
     @IBOutlet var sceneView: ARSCNView!
-    
+    var timer: Timer?
+    var counter = 0
     public override func viewDidLoad() {
         super.viewDidLoad()
         
         sceneView = ARSCNView(frame: .zero)
         self.view = sceneView
         self.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        
+        
+        
+        timer =  Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (t) in
+            self.updateGame()}
         
         
         // Set the view's delegate
@@ -30,29 +37,18 @@ public class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/Kayak.dae")!
-        /*
-        let walrusHead = scene.rootNode.childNode(withName: "WalrusHead-0", recursively: true)!
-        let walrusBody = scene.rootNode.childNode(withName: "WalrusBody-0", recursively: true)!
-        let simdWalrusHeadPosition = walrusHead.simdPosition
-        let simdWalrusBodyPosition = walrusBody.simdPosition
-        let difference = simdWalrusHeadPosition - simdWalrusBodyPosition
-        walrusHead.simdPosition = difference
-        walrusBody.addChildNode(walrusHead)
-        let billboardConstraint = SCNBillboardConstraint()
-        billboardConstraint.freeAxes = .Y
-        //walrusHead.constraints = [billboardConstraint]
-        //let walrusHead2 = SCNNode()
-        //walrusHead2.simdPosition = walrusHead.simdPosition
-        let cameraNode = sceneView.pointOfView!
-        let lookAtConstraint = SCNLookAtConstraint( target: cameraNode)
-        lookAtConstraint.isGimbalLockEnabled = true
-        walrusHead.constraints = [lookAtConstraint]
-        */
-        
+
+        let fish = SCNScene(named: "art.scnassets/Fish.dae")!.rootNode
        
+        let seagull = SCNScene(named: "art.scnassets/Seagull.dae")!.rootNode
         
+        scene.rootNode.addChildNode(seagull)
         
-    
+        scene.rootNode.addChildNode(fish)
+        
+        seagull.simdPosition = float3(1, 0, 1)
+        fish.simdPosition = float3(0, 0 , -6)
+        
         // Set the scene to the view
         sceneView.scene = scene
         
@@ -65,6 +61,20 @@ public class ViewController: UIViewController, ARSCNViewDelegate {
         oceanSound.loops = true
         let audioPlayer2 = SCNAudioPlayer(source: musicSound)
         scene.rootNode.addAudioPlayer(audioPlayer2)
+        
+        
+        
+    }
+    
+    func updateGame(){
+        if(counter%15 == 0){
+            let dolphinsound = SCNAudioSource(fileNamed: "art.scnassets/Dolphins.wav")!
+            let audioPlayer3 = SCNAudioPlayer(source: dolphinsound)
+            sceneView.scene.rootNode.addAudioPlayer(audioPlayer3)
+            print("sound played")
+        }
+        counter = counter + 1
+
     }
     
     public override func viewWillAppear(_ animated: Bool) {
@@ -130,4 +140,7 @@ relativeTo referenceNode: SCNNode)
     let updatedTransform = matrix_multiply(referenceNodeTransform, translationMatrix)
     node.transform = SCNMatrix4(updatedTransform)
 }
+
+
+
 
