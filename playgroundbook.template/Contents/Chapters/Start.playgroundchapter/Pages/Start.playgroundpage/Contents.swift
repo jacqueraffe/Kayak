d97e8fb00f6f88cb019ed11839ed57c33666b93c
Jacqueline Palevich <jacqueline.palevich@gmail.com>
2019-03-24 13:07:33 -0700

    Done

--- playgroundbook.template/Contents/Chapters/Start.playgroundchapter/Pages/Start.playgroundpage/Contents.swift
+++ playgroundbook.template/Contents/Chapters/Start.playgroundchapter/Pages/Start.playgroundpage/Contents.swift
@@ -1,5 +1,25 @@
+//#-hidden-code
 import PlaygroundSupport
 
 
 // Present the view controller in the Live View window
 PlaygroundPage.current.liveView = ViewController()
+//#-end-hidden-code
+/*:
+ # Welcome to Nature Watching
+ 
+ - Author: Jacqueline Palevich
+ 
+ Enjoy looking at a whale, walrus, and seagull in this relaxing interactive playground.
+
+ ## Instructions:
+ 
+ - Hold device vertically
+ 
+ - Tap Run My Code
+ 
+ - Turn on sound
+ 
+ - Then walk and look around to see all the animals.
+
+ */
